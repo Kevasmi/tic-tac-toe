e4fd2532a7bf5ee766f54a931864063740831449
Richard Lasco <lasco9@gmail.com>
2022-04-21 13:49:17 -0500

    Began work on gameFlow module

--- app.js
+++ app.js
@@ -12,18 +12,18 @@ const player1 = playerFactory('Player 1');
     const gameBoard = {
         gameBoardArr: [1,2,3,4,5,6,7,8,9],
         init: function init() {
-            let cache = this.cacheDom()
+            let cache = this.cacheDom();
             this.render(this.gameBoardArr, cache);
         },
         cacheDom: function cacheDom() {
-            const gameBoardContainer = document.querySelector('.gameboard')
-            const squares = document.querySelectorAll('.square')
-            return {squares, gameBoardContainer}
+            const gameBoardContainer = document.querySelector('.gameboard');
+            const squares = document.querySelectorAll('.square');
+            return {squares, gameBoardContainer};
         },
         render: function render(gameBoardArr, cache) {
             gameBoardArr.forEach(number => {
                 const square = document.createElement('div');
-                square.classList.add('square')
+                square.classList.add('square');
                 square.textContent = number;
                 cache.gameBoardContainer.appendChild(square);
             });
@@ -31,6 +31,24 @@ const player1 = playerFactory('Player 1');
     };
 
     return gameBoard.init()
+
+})();
+
+(function() {
+
+    const gameFlow = {
+        init: function init() {
+            let squares = gameBoard.init().cache.squares
+            console.log('squares')
+            this.bindEvents(squares)
+        },
+        bindEvents: function bindEvents(squares) {
+
+        }
+    }
+
+
+    return gameFlow.init()
 
 })();
 
