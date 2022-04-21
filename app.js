a73ba9932e8a3da6cb561f48597db0dca0dd96d7
Richard Lasco <lasco9@gmail.com>
2022-04-20 20:48:53 -0500

    Added gameBoard module

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('hello world!')
+(function() {
+
+    const gameBoard = {
+        init: function init() {
+            let squares = this.cacheDom();
+            // this.cacheDom();
+            this.bindEvents(squares);
+        },
+        cacheDom: function cacheDom() {
+            const squares = document.querySelectorAll('.square')
+            return squares
+        },
+        bindEvents: function bindEvents(squares) {
+            squares.forEach(square => {
+                square.addEventListener('click', () => {this.clickFunc()})  
+            });
+        },
+        clickFunc: ()  => {console.log('hello world')}
+    };
+
+    return gameBoard.init()
+
+})();
+
