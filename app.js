6d4c75d487ceb59cb826e9e27795d9761f23aed6
Richard Lasco <lasco9@gmail.com>
2022-04-21 13:06:26 -0500

    Added render functionality to gameBoard module

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
+const playerFactory = (name) => {
+    let array = [];
+    return {name, array}
+};
+
+const player1 = playerFactory('Player 1');
+
+// console.log(player1.array);
+
 (function() {
 
     const gameBoard = {
+        gameBoardArr: [1,2,3,4,5,6,7,8,9],
         init: function init() {
-            let squares = this.cacheDom();
-            // this.cacheDom();
-            this.bindEvents(squares);
+            let cache = this.cacheDom()
+            this.render(this.gameBoardArr, cache);
         },
         cacheDom: function cacheDom() {
+            const gameBoardContainer = document.querySelector('.gameboard')
             const squares = document.querySelectorAll('.square')
-            return squares
+            return {squares, gameBoardContainer}
         },
-        bindEvents: function bindEvents(squares) {
-            squares.forEach(square => {
-                square.addEventListener('click', () => {this.clickFunc()})  
+        render: function render(gameBoardArr, cache) {
+            gameBoardArr.forEach(number => {
+                const square = document.createElement('div');
+                square.classList.add('square')
+                square.textContent = number;
+                cache.gameBoardContainer.appendChild(square);
             });
         },
-        clickFunc: ()  => {console.log('hello world')}
     };
 
     return gameBoard.init()
 
 })();
+
 
